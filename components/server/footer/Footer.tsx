2deed1e2d13e6227e7f240d24f23dc1c6a538fb7
Sithi5 <ma.sithis@directbrico.com>
2023-01-31 15:56:39 +0100

    [feat]: Update footer

--- components/server/footer/Footer.tsx
+++ components/server/footer/Footer.tsx
@@ -11,11 +11,12 @@ export default function Footer() {
             <footer
                 className={'absolute bottom-0 w-full z-50  h-' + footerHeight}
             >
-                <div className="flex justify-center p-4">
+                <div className="flex flex-col justify-center p-4 items-center">
                     <p className="text-grey">
                         Made with <Emoji symbol="❤️" label="heart" /> by Malo
                         Boucé
                     </p>
+                    <p className="text-grey">(powered by next.js 13)</p>
                 </div>
             </footer>
         </>
