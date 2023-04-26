6237d3572200db2c05984bcd3625a0dba1c24000
Sithi5 <ma.sithis@directbrico.com>
2023-04-26 21:25:55 +0200

    [undo]:  remove analytic because bug.
    Need further tests

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -2,7 +2,6 @@ import { Navbar, ScrollToTopButton } from 'components/client/';
 import { Footer } from 'components/server';
 import { getTranslator, locales, ValidLocale } from 'i18n';
 import 'styles/globals.css';
-import { Analytics } from '@vercel/analytics/react';
 
 export async function generateStaticParams() {
     return locales.map((locale) => ({
@@ -45,7 +44,6 @@ export default async function RootLayout(props: RootProps) {
                     />
                 </header>
                 <main>{children}</main>
-                <Analytics />
                 <Footer translator={translator} />
                 <ScrollToTopButton />
             </body>
